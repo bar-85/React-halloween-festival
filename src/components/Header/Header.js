94815458645287b92d4a7f7a0d83132ccf7c27fa
bar-85 <bajwolskabarbara@gmail.com>
2022-10-21 22:34:39 +0200

    add styled components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 import { FaBars, FaTimes } from 'react-icons/fa'
-import './Header.css'
-import { Hamburger } from './Header.styles';
+
+import { Hamburger, Wrapper, Nav } from './Header.styles'
 import LogoHeader from '../Logo/LogoHeader'
 import SocialIcon from '../SocialIcon/SocialIcon'
+
 
 
 const Header = () => {
@@ -12,20 +13,26 @@ const Header = () => {
 	const handleClick = () => setClick(!click)
 
 	const closeMenu = () => setClick(false)
-	
+
 	return (
-		<div className='header'>
-			<LogoHeader></LogoHeader>
+		<Wrapper>
+			<LogoHeader />
 			<Hamburger onClick={handleClick}>
 				{click ? <FaTimes size={32} style={{ color: 'white' }} /> : <FaBars size={32} style={{ color: 'white' }} />}
 			</Hamburger>
-			<div className={click ? 'header-menu active' : 'header-menu'}>
-				<Link to='/' className='header-link' onClick={closeMenu}>Strona Główna</Link>
-				<Link to='/the-tickets' className='header-link' onClick={closeMenu}>Bilety</Link>
-				<Link className='header-link' onClick={closeMenu}>Quiz</Link>
-				<SocialIcon></SocialIcon>
-			</div>
-		</div>
+			<Nav className={click ? 'header-menu active' : 'header-menu'}>
+				<Link to='/' onClick={closeMenu}>
+					<li>Strona Główna</li>
+				</Link>
+				<Link to='/the-tickets' onClick={closeMenu}>
+					<li>Bilety</li>
+				</Link>
+				<Link to='/Quiz' onClick={closeMenu}>
+					<li>Quiz</li>
+				</Link>
+				<SocialIcon />
+			</Nav>
+		</Wrapper>
 	)
 }
 
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
 
+
+export const Wrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+
+	@media screen and (max-width: 900px) {
+		max-width: 100%;
+	}
+`
+
 export const Hamburger = styled.div`
 	display: none;
 
@@ -9,5 +19,51 @@ export const Hamburger = styled.div`
 		display: block;
 		margin-top: 1rem;
 		z-index: 20;
+	}
+`
+export const Nav = styled.div`
+	&.header-menu {
+		align-items: center;
+		list-style: none;
+		display: flex;
+		gap: 2rem;
+	}
+
+	li {
+		cursor: pointer;
+		color: white;
+	}
+
+	li:hover {
+		cursor: pointer;
+		color: orange;
+	}
+
+	@media screen and (max-width: 900px) {
+		&.header-menu {
+			position: fixed;
+			right: -100%;
+			top: 100px;
+			flex-direction: column;
+			background-color: rgba(76, 19, 106, 0.966);
+			width: 50%;
+			height: 55vh;
+			z-index: 999;
+			border-top-left-radius: 40px;
+			border-bottom-left-radius: 40px;
+			border: 2px solid orange;
+			text-align: center;
+			transition: 0.3s;
+		}
+
+		&.header-menu.active {
+			right: -3px;
+		}
+
+		li {
+			margin-top: 2.5rem;
+		}
+
+	
 	}
 `
