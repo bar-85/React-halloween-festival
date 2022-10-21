73b9367360f222293b4770fcb36022d19419dfec
bar-85 <bajwolskabarbara@gmail.com>
2022-10-21 21:25:41 +0200

    add styled-components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom";
-
 import { FaBars, FaTimes } from 'react-icons/fa'
 import './Header.css'
-
-import navIcon1 from '../../assets/nav-icon1.svg'
-import navIcon2 from '../../assets/nav-icon2.svg'
-import navIcon3 from '../../assets/nav-icon3.svg'
-import { SocialIcon, Hamburger } from './Header.styles';
+import { Hamburger } from './Header.styles';
 import LogoHeader from '../Logo/LogoHeader'
+import SocialIcon from '../SocialIcon/SocialIcon'
 
 
 const Header = () => {
@@ -27,17 +23,7 @@ const Header = () => {
 				<Link to='/' className='header-link' onClick={closeMenu}>Strona Główna</Link>
 				<Link to='/the-tickets' className='header-link' onClick={closeMenu}>Bilety</Link>
 				<Link className='header-link' onClick={closeMenu}>Quiz</Link>
-				<SocialIcon>
-					<a href='/'>
-						<img src={navIcon1} alt='' />
-					</a>
-					<a href='/'>
-						<img src={navIcon2} alt='' />
-					</a>
-					<a href='/'>
-						<img src={navIcon3} alt='' />
-					</a>
-				</SocialIcon>
+				<SocialIcon></SocialIcon>
 			</div>
 		</div>
 	)
---
+++ src/components/SocialIcon/SocialIcon.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { SocialIconStyles } from '../SocialIcon/SocialIcon.Styles'
+import navIcon1 from '../../assets/nav-icon1.svg'
+import navIcon2 from '../../assets/nav-icon2.svg'
+import navIcon3 from '../../assets/nav-icon3.svg'
+
+const SocialIcon = () => {
+	return (
+		<SocialIconStyles>
+			<a href='/'>
+				<img src={navIcon1} alt='' />
+			</a>
+			<a href='/'>
+				<img src={navIcon2} alt='' />
+			</a>
+			<a href='/'>
+				<img src={navIcon3} alt='' />
+			</a>
+		</SocialIconStyles>
+	)
+}
+
+export default SocialIcon
