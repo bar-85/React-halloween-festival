d7eebcea7dfbd51ed1f512e7931d58ddbb740352
bar-85 <bajwolskabarbara@gmail.com>
2022-10-24 15:33:33 +0200

    add

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,75 +1,52 @@
+import './About.css'
+import Card from '../Card/Card'
+import { motion } from 'framer-motion'
+import ButtonContact from '../Buttons/ButtonContact'
+import Blur from '../Blur/Blur'
+import imgOne from '../../assets/1.jpg'
+import imgTwo from '../../assets/2.jpg'
+import imgThree from '../../assets/3.jpg'
+import { AboutStyled, Awesome, Cards } from './About.styles'
 
-import "./About.css";
-import Card from "../Card/Card";
-import { motion } from "framer-motion";
-import ButtonContact from "../Buttons/ButtonContact";
-import Blur from "../Blur/Blur";
-import imgOne from "../../assets/1.jpg"
-import imgTwo from "../../assets/2.jpg"
-import imgThree from "../../assets/3.jpg"
+const About = () => {
+	const transition = {
+		duration: 1,
+		type: 'spring',
+	}
 
+	return (
+		<div>
+			<AboutStyled>
+				<Awesome>
+					<span>UPIORNE ZABAWY</span>
+					<span>których nie zapomnisz!</span>
+					<span>
+						Przygotuj się na noc, pełną wrażeń!
+						<br />
+						Zarejestruj się na Halloween Festival 2023
+					</span>
+					<a href={'/'}>
+						<ButtonContact />
+					</a>
+					<Blur />
+				</Awesome>
 
-const Services = () => {
-  const transition = {
-    duration: 1,
-    type: "spring",
-  };
+				<Cards>
+					<motion.div initial={{ left: '40rem' }} whileInView={{ left: '30rem' }} transition={transition}>
+						<Card picture={imgOne} />
+					</motion.div>
 
-  return (
-    <div className="services" id="services">
+					<motion.div initial={{ left: '3rem', top: '12rem' }} whileInView={{ left: '8rem' }} transition={transition}>
+						<Card picture={imgThree} />
+					</motion.div>
 
-      <div className="awesome">
- 
-        <span>UPIORNE ZABAWY</span>
-        <span>których nie zapomnisz!</span>
-        <spane>
-        Przygotuj się na noc, pełną wrażeń!
-          <br />
-          Zarejestruj się na Halloween Festival 2023
-        </spane>
-        <a href={'/'} >
-          <ButtonContact />
-        </a>
-        <Blur />
-      </div>
-  
-      <div className="cards">
+					<motion.div initial={{ top: '19rem', left: '40rem' }} whileInView={{ left: '28rem' }} transition={transition}>
+						<Card picture={imgTwo} color='rgba(252, 166, 31, 0.45)' />
+					</motion.div>
+				</Cards>
+			</AboutStyled>
+		</div>
+	)
+}
 
-        <motion.div
-          initial={{ left: "40rem" }}
-          whileInView={{ left: "30rem" }}
-          transition={transition}
-        >
-          <Card
-            picture={imgOne}
-          />
-        </motion.div>
-
-        <motion.div
-          initial={{ left: "3rem", top: "12rem" }}
-          whileInView={{ left: "8rem" }}
-          transition={transition}
-        >
-          <Card
-            picture={imgThree}
-          />
-        </motion.div>
-
-        <motion.div
-          initial={{ top: "19rem", left: "40rem" }}
-          whileInView={{ left: "28rem" }}
-          transition={transition}
-        >
-          <Card
-            picture={imgTwo}
-
-            color="rgba(252, 166, 31, 0.45)"
-          />
-        </motion.div>
-       
-      </div>
-    </div>
-  );
-};
-
-export default Services;
+export default About
---
+++ src/components/About/About.styles.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const AboutStyled = styled.div`
+	padding: 0 3rem 0 3rem;
+	display: flex;
+	height: 30rem;
+	margin-bottom: 8rem;
+	margin-top: 9rem;
+	margin-bottom: 13rem;
+	padding-top: 3rem;
+
+	@media screen and (max-width: 900px) {
+		margin-top: 0;
+		flex-direction: column;
+		gap: 5rem;
+		height: 66rem;
+		padding: 0;
+	}
+`
+
+export const Awesome = styled.div`
+	display: flex;
+	flex-direction: column;
+	position: relative;
+
+	span:nth-child(1) {
+		color: black;
+		font-size: 2.5rem;
+		font-weight: bold;
+	}
+	span:nth-child(2) {
+		color: orange;
+		font-size: 2.5rem;
+		font-weight: bold;
+	}
+	span:nth-child(3) {
+		color: gray;
+		font-size: 14px;
+		margin-top: 1rem;
+		margin-bottom: 2rem;
+	}
+
+	@media screen and (max-width: 900px) {
+		align-items: center;
+
+		span:nth-child(1) {
+			font-size: 1.5rem;
+		}
+
+		span:nth-child(2) {
+			font-size: 1.5rem;
+			margin: 1rem;
+		}
+		span:nth-child(3) {
+			color: gray;
+			font-size: 14px;
+		}
+	}
+`
+
+export const Cards = styled.div`
+	position: relative;
+
+	* {
+		position: absolute;
+	}
+
+	@media screen and (max-width: 900px) {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+
+		* {
+			position: static;
+		}
+	}
+`
