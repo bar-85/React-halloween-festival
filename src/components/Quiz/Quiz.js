af948ac372a32dbd3a01dcadca7c205484dafde3
bar-85 <bajwolskabarbara@gmail.com>
2022-10-30 19:58:26 +0100

    add

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,66 +3,10 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { QuizStyled, AppStyled, Title, Question, QuestionTitle, Answer, ScoreTitle } from './Quiz.styles'
 import backImg from '../../assets/back.png'
+import { questions } from '../../data/quizData'
 
 
 const Quiz = () => {
-	const questions = [
-		{
-			questionText: 'Kiedy obchodzimy Halloween?',
-			answerOptions: [
-				{ answerText: '1 kwietnia', isCorrect: false },
-				{ answerText: '2 listopada', isCorrect: false },
-				{ answerText: '31 października', isCorrect: true },
-				{ answerText: '6 grudnia', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Co jest symbolem Halloween?',
-			answerOptions: [
-				{ answerText: 'Bób', isCorrect: false },
-				{ answerText: 'Dynia', isCorrect: true },
-				{ answerText: 'Gruszka', isCorrect: false },
-				{ answerText: 'Śliwka', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Halloween najpopularniejsze jest w...?',
-			answerOptions: [
-				{ answerText: 'Polsce', isCorrect: false },
-				{ answerText: 'Holnadii', isCorrect: false },
-				{ answerText: 'USA', isCorrect: true },
-				{ answerText: 'Turcji', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Co zbierają dzieci w Halloween?',
-			answerOptions: [
-				{ answerText: 'pieniądze', isCorrect: false },
-				{ answerText: 'kukurydzę', isCorrect: false },
-				{ answerText: 'narkotyki', isCorrect: false },
-				{ answerText: 'słodycze', isCorrect: true },
-			],
-		},
-		{
-			questionText: 'Cukierek albo...???',
-			answerOptions: [
-				{ answerText: 'gorączka', isCorrect: false },
-				{ answerText: 'granat', isCorrect: false },
-				{ answerText: 'psikus', isCorrect: true },
-				{ answerText: 'pizza', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Polski odpowiednik Halloween?',
-			answerOptions: [
-				{ answerText: 'Wielkanoc', isCorrect: false },
-				{ answerText: 'Andrzejki', isCorrect: false },
-				{ answerText: 'Boże Narodzenie', isCorrect: false },
-				{ answerText: 'Zaduszki', isCorrect: true },
-			],
-		},
-	]
-
 	const [currentQuestion, setCurrentQuestion] = useState(0)
 	const [showScore, setShowScore] = useState(false)
 	const [score, setScore] = useState(0)
---
+++ src/data/quizData.js
@@ -0,0 +1,56 @@
+export const questions = [
+	{
+		questionText: 'Kiedy obchodzimy Halloween?',
+		answerOptions: [
+			{ answerText: '1 kwietnia', isCorrect: false },
+			{ answerText: '2 listopada', isCorrect: false },
+			{ answerText: '31 października', isCorrect: true },
+			{ answerText: '6 grudnia', isCorrect: false },
+		],
+	},
+	{
+		questionText: 'Co jest symbolem Halloween?',
+		answerOptions: [
+			{ answerText: 'Bób', isCorrect: false },
+			{ answerText: 'Dynia', isCorrect: true },
+			{ answerText: 'Gruszka', isCorrect: false },
+			{ answerText: 'Śliwka', isCorrect: false },
+		],
+	},
+	{
+		questionText: 'Halloween najpopularniejsze jest w...?',
+		answerOptions: [
+			{ answerText: 'Polsce', isCorrect: false },
+			{ answerText: 'Holnadii', isCorrect: false },
+			{ answerText: 'USA', isCorrect: true },
+			{ answerText: 'Turcji', isCorrect: false },
+		],
+	},
+	{
+		questionText: 'Co zbierają dzieci w Halloween?',
+		answerOptions: [
+			{ answerText: 'pieniądze', isCorrect: false },
+			{ answerText: 'kukurydzę', isCorrect: false },
+			{ answerText: 'narkotyki', isCorrect: false },
+			{ answerText: 'słodycze', isCorrect: true },
+		],
+	},
+	{
+		questionText: 'Cukierek albo...???',
+		answerOptions: [
+			{ answerText: 'gorączka', isCorrect: false },
+			{ answerText: 'granat', isCorrect: false },
+			{ answerText: 'psikus', isCorrect: true },
+			{ answerText: 'pizza', isCorrect: false },
+		],
+	},
+	{
+		questionText: 'Polski odpowiednik Halloween?',
+		answerOptions: [
+			{ answerText: 'Wielkanoc', isCorrect: false },
+			{ answerText: 'Andrzejki', isCorrect: false },
+			{ answerText: 'Boże Narodzenie', isCorrect: false },
+			{ answerText: 'Zaduszki', isCorrect: true },
+		],
+	},
+]
