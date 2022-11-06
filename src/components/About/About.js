a831902aafd881f458d8cba9e08e5ca0835e7df8
bar-85 <bajwolskabarbara@gmail.com>
2022-11-06 23:49:48 +0100

    add about.js and about.styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import cat from '../../assets/cat1.png'
+import pumpkin from '../../assets/pumpkin1.png'
+import { Reasons, LeftSide, RightSide, Details } from './About.styles'
 
 const About = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<>
+			<Reasons>
+				<LeftSide>
+					<img src={cat} alt='halloween zdjęcie' className='img' />
+				</LeftSide>
+				<RightSide>
+					<div className='title'>
+						<span>Dlaczego</span>
+						<span> nie może Cię zabraknąć?</span>
+						<span></span>
+					</div>
+					<Details>
+						<div>
+							<img src={pumpkin} alt='zdjęcie dyni' />
+							<span>Wspaniała zabawa dla całej rodziny</span>
+						</div>
+						<div>
+							<img src={pumpkin} alt='zdjęcie dyni' />
+							<span>Imprezy towarzyszące (konkursy, wystawy, kino i wiele innych...)</span>
+						</div>
+						<div>
+							<img src={pumpkin} alt='zdjęcie dyni' />
+							<span>Bajkowe, dopracowane w najmniejszych detalach dekoracje Halloween</span>
+						</div>
+						<div>
+							<img src={pumpkin} alt='zdjęcie dyni' />
+							<span>Prawdziwe święto dla miłośników mocnych wrażeń!</span>
+						</div>
+					</Details>
+				</RightSide>
+			</Reasons>
+		</>
+	)
 }
 
 export default About
--- src/components/About/About.styles.js
+++ src/components/About/About.styles.js
@@ -1,10 +1,113 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div`
+export const Reasons = styled.div`
+	margin-top: 3rem;
 	display: flex;
-	justify-content: space-between;
-	
+	padding: 2rem;
+	gap: 2rem;
+
+	@media screen and (max-width: 1250px) {
+		margin-top: 10rem;
+		padding: 2rem;
+		gap: 5rem;
+	}
+
 	@media screen and (max-width: 900px) {
-		flex-direction: column;
+		margin-top: 0;
+		flex-direction: column-reverse;
+	}
+`
+export const LeftSide = styled.div`
+	flex: 1 1;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	img {
+		width: 60%;
+	}
+
+	@media screen and (max-width: 900px) {
+		img {
+			width: 50%;
+		}
+	}
+`
+export const RightSide = styled.div`
+	flex: 1 1;
+	gap: 1rem;
+	display: flex;
+	flex-direction: column;
+	font-size: 2.5rem;
+
+	.title > span:nth-child(1) {
+		font-weight: bold;
+		color: white;
+	}
+
+	.title > span:nth-child(2) {
+		color: orange;
+		font-weight: bold;
+	}
+
+	@media screen and (max-width: 1250px) {
+		font-size: 2.5rem;
+	}
+
+	@media screen and (max-width: 900px) {
+		padding-left: 5rem;
+		font-size: 2.5rem;
+		margin-top: -4rem;
+	}
+	@media screen and (max-width: 768px) {
+		padding-left: 2rem;
+		font-size: 2rem;
+	}
+
+	@media screen and (max-width: 480px) {
+		padding-left: 1rem;
+		font-size: 1.5rem;
+	}
+
+	@media screen and (max-width: 320px) {
+	}
+`
+export const Details = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+
+	div {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	div > span {
+		font-size: 1rem;
+		color: white;
+	}
+
+	div > img {
+		width: auto;
+		height: 4rem;
+	}
+
+	@media screen and (max-width: 480px) {
+		div > span {
+			font-size: 0.8rem;
+		}
+
+		div > img {
+			height: 2rem;
+		}
+	}
+
+	@media screen and (max-width: 320px) {
+		div > span {
+			font-size: 0.75rem;
+		}
+
+		
 	}
 `
