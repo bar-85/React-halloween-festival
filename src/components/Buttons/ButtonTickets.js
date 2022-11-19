e24bb374b23b19d097fb66fada56a3cf6ff2cbcb
bar-85 <bajwolskabarbara@gmail.com>
2022-11-19 15:06:05 +0100

    button improvement

--- src/components/Buttons/ButtonTickets.js
+++ src/components/Buttons/ButtonTickets.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ButtonTicketsStyled } from './Button.styles'
 
 const ButtonTickets = () => {
-	return <ButtonTicketsStyled>Bilety</ButtonTicketsStyled>
+	return <ButtonTicketsStyled>Wróć</ButtonTicketsStyled>
 }
 
 export default ButtonTickets
