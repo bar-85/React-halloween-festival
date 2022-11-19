a4b0aa2fef271581b6940f6d3b9846ab87b589d3
bar-85 <bajwolskabarbara@gmail.com>
2022-11-19 15:28:36 +0100

    add Header and Footer

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Contact from './views/Contact/Contact'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Wrapper } from './App.styles'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
 	return (
 		<BrowserRouter>
 			<ScrollToTop>
 				<Wrapper>
+					<Header />
 					<Routes>
 						<Route path='/' element={<TheHome />} />
 						<Route path='/the-tickets' element={<TheTickets />} />
 						<Route path='/quiz' element={<Quiz />} />
 						<Route path='/contact' element={<Contact />} />
 					</Routes>
+					<Footer />
 				</Wrapper>
 			</ScrollToTop>
 		</BrowserRouter>
